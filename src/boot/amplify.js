fa40afc627e11148af550ab2fd07e8621e21e2e6
marknigh70@gmail.com <marknig70@gmail.com>
2023-12-12 14:39:26 -0600

    added graphql amplify.configure to boot file to help with no endpoint error when deployed.

--- src/boot/amplify.js
+++ src/boot/amplify.js
@@ -1,6 +1,6 @@
 import { boot } from 'quasar/wrappers'
 import { Amplify, Auth, Hub } from 'aws-amplify'
-import awsconfig from '../aws-exports'
+import awsmobile from '../aws-exports'
 import { useUserStore } from '../stores/user'
 
 const userStore = useUserStore()
@@ -8,13 +8,11 @@ const userStore = useUserStore()
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async (/* { app, router, ... } */) => {
-  Amplify.configure(awsconfig)
+  Amplify.configure(awsmobile)
   Amplify.configure({
-    API: {
-      graphql_endpoint: 'https://tp3bu26tmfal7kvtvsgsvo2vpy.appsync-api.us-east-1.amazonaws.com/graphql',
-      aws_appsync_region: 'us-east-1',
-      aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS'
-    }
+    aws_appsync_graphqlEndpoint: 'https://tp3bu26tmfal7kvtvsgsvo2vpy.appsync-api.us-east-1.amazonaws.com/graphql',
+    aws_appsync_region: 'us-east-1',
+    aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS'
   })
 
   Hub.listen('auth', ({ payload: { event, data } }) => {
