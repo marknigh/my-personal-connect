4525646bbce7031d37d914c0d0fbaaa3f92d9483
marknigh70@gmail.com <marknig70@gmail.com>
2023-12-12 13:08:44 -0600

    boot/amplify.js syntax error fix

--- src/boot/amplify.js
+++ src/boot/amplify.js
@@ -12,10 +12,9 @@ export default boot(async (/* { app, router, ... } */) => {
   Amplify.configure({
     API: {
       graphql_endpoint: 'https://tp3bu26tmfal7kvtvsgsvo2vpy.appsync-api.us-east-1.amazonaws.com/graphql',
-      aws_appsync_region: "us-east-1",
-      aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS"
-      }
-    })
+      aws_appsync_region: 'us-east-1',
+      aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS'
+    }
   })
 
   Hub.listen('auth', ({ payload: { event, data } }) => {
